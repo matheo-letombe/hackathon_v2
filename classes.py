cdb2039d74b873b0bcdb92c78b9723b496fdb15a
Aymar Smyej <aymar.smyej@etu.minesparis.psl.eu>
2026-01-30 11:48:40 +0100

    Re commit

--- classes.py
+++ classes.py
@@ -5,6 +5,8 @@ class Grass:
         self.state = state 
         self.eaten = eaten
         self.time_growth = time_growth
+
+
 class Animal:
     position:tuple
     age:int
@@ -53,12 +55,16 @@ class Sheep(Animal):
     def move(self)->None:
         if self.move_down_possible() and map[self.position[0]][self.position[1]-1][0].state :
             self.move_down()
+            self.eat_grass()
         if self.move_up_possible() and map[self.position[0]][self.position[1]+1][0].state:
             self.move_up()
+            self.eat_grass()
         if self.move_right_possible() and map[self.position[0]+1][self.position[1]][0].state:
             self.move_right()
+            self.eat_grass()
         if self.move_left_possible() and map[self.position[0]-1][self.position[1]][0].state:
             self.move_left()
+            self.eat_grass()
         else:
             L = [self.move_down(), self.move_up(), self.move_left(), self.move_right()]
             for i in range(4,0,-1):
@@ -69,7 +75,11 @@ class Sheep(Animal):
                 else:
                     L.remove(L[r])
             
-                
+    def eat_grass(self) -> None:
+        herbe = map[self.position[0]][self.position[1]][0]
+        herbe.eaten = True
+        herbe.time_growth = 0
+        self.energy += 20   
                
 class Wolf(Animal):
     pass
